344938d31234e1079bff8cc5115f160290196421
tigerfarm <tigerfarm@gmail.com>
2018-06-25 14:01:55 -0700

    Update

--- deleteMapItem.php
+++ deleteMapItem.php
@@ -18,6 +18,7 @@ class HTTPRequester {
     }
 
 }
+
 // -----------------------------------------------------------------------------
 $counterName = "counterd";  // The Sync Map Key value used as the counter name.
 echo "+ Delete counter: " . $counterName;
@@ -33,12 +34,27 @@ $data = array(
 );
 $http = new HTTPRequester();
 $response = $http->HTTPDelete($accountSid, $authToken, $url, $data);
-echo "\xA+ Response :{$response}:";
+// echo "\xA+ Response :{$response}:";
+$end = stripos($response, "Access");
+$httpResponse = substr($response, 0, $end - 1);
+// echo "\xA+ Index of 204:" . stripos($httpResponse, "204");
+if (stripos($httpResponse, "204") != null) {
+    // HTTP/1.1 204 NO CONTENT
+    // 204 No Content - The server has fulfilled the request, however does not return content.
+    echo "\xA++ Deleted counter, Key = " . $counterName . ".\xA";
+    return;
+}
+// HTTP/1.1 404 NOT FOUND
+// 404 NOT FOUND - delete failed.
+// {"code": 20404, "message": "The requested resource
+echo "\xA++ HTTP Response code: {$httpResponse}";
 $start = stripos($response, "{");
 $jsonLength = strlen($response) - $start;
 $jsonOnly = substr( $response, $start, $jsonLength );
-// echo "\xA+ JSON :{$jsonOnly}:";
+// echo "\xA+ JSON response:{$jsonOnly}:";
 $jsonResponse = json_decode($jsonOnly);
 // -----------------------------------------------------------------------------
-echo "\xA++ Created: Key = " . $jsonResponse->key . "\xA";
+echo "\xA++ Error message: " . $jsonResponse->message . "\xA";
+
+// -----------------------------------------------------------------------------
 ?>
