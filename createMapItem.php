a88bd87009ed9c89427ba3adc99d7feac6068ed4
tigerfarm <tigerfarm@gmail.com>
2018-06-24 19:27:06 -0700

    Update

--- createMapItem.php
+++ createMapItem.php
@@ -18,7 +18,7 @@ class HTTPRequester {
 
 }
 
-echo '+++ Start.';
+// echo '+++ Start.';
 $accountSid = getenv("ACCOUNT_SID");
 $authToken = getenv('AUTH_TOKEN');
 $syncServieSid = getenv('SYNC_SERVICE_SID');
@@ -27,13 +27,24 @@ $syncMapName = getenv('SYNC_MAP_NAME');
 $url = "https://sync.twilio.com/v1/Services/{$syncServieSid}/Maps/{$syncMapName}/Items";
 $data = array(
     'Ttl' => 0, // 0 - never expires
-    'Key' => "counterb",
-    'Data' => '{"counter": 2}'
+    'Key' => "counterd",
+    'Data' => '{"counter": 4}'
 );
 
-echo "\xA++ The request URL: ", $url;
+// echo "\xA++ The request URL: ", $url;
 $http = new HTTPRequester();
 $response = $http->HTTPPost($accountSid, $authToken, $url, $data);
-echo "\xA+ Response: {$response}";
-echo '\xA+++ Exit.\xA';
+// substr ( string $string , int $start [, int $length ] )
+// stripos($response, "$substring"{")
+// strlen()
+$start = stripos($response, "{");
+$jsonLength = strlen($response) - $start;
+// echo "\xA+ JSON start = {$start} JSON length = {$jsonLength}";
+$jsonOnly = substr( $response, $start, $jsonLength );
+// echo "\xA+ Response :{$response}:";
+echo "\xA+ JSON :{$jsonOnly}:";
+
+$jsonResponse = json_decode($jsonOnly);
+echo "\xA++ Created: Key = " . $jsonResponse->key;
+echo "\xA+++ Exit.\xA";
 ?>
