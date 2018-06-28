ab0301391b0731527029db8d82eb91e476b749e2
tigerfarm <tigerfarm@gmail.com>
2018-06-28 12:44:08 -0700

    Update

--- setvars.sh
+++ setvars.sh
@@ -3,8 +3,10 @@ echo "+++ Set variables."
 ACCOUNT_SID=your_account_SID
 AUTH_TOKEN=your_account_auth_token
 SYNC_SERVICE_SID=your_sync_service_SID
+SYNC_MAP_NAME=counters
 export ACCOUNT_SID
 export AUTH_TOKEN
 export SYNC_SERVICE_SID
+export SYNC_MAP_NAME
 php echoVars.php
 # ------------------------------------------------------------------------------
