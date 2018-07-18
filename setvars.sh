14a63dd61f219eb4d44deba227d656141416d3d6
tigerfarm <tigerfarm@gmail.com>
2018-07-18 13:15:23 -0700

    Update

--- setvars.sh
+++ setvars.sh
@@ -3,6 +3,17 @@ echo "+++ Set variables."
 export ACCOUNT_SID=your_account_SID
 export AUTH_TOKEN=your_account_auth_token
 export SYNC_SERVICE_SID=your_sync_service_SID
+export SYNC_MAP_NAME=your_sync_mapname_or_groupsms_phonenumber
+export NOTIFY_SERVICE_SID=ISa62b58313e737b6e9d41d19eeef9c32e
+# -------
+# For Group SMS:
+export SEND_ACCOUNT_SID=your_account_SID
+export SEND_AUTH_TOKEN=your_account_auth_token
+export PHONE_NUMBER_1=your_test_phonenumber1
+export PHONE_NUMBER_2=your_test_phonenumber2
+export PHONE_NUMBER_3=your_test_phonenumber3
+export PHONE_NUMBER_4=your_test_phonenumber4
+
 export SYNC_MAP_NAME=counters
-php echoVars.php
+node echoVarsGs.js
 # ------------------------------------------------------------------------------
