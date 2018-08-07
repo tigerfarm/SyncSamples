97e4e1c113854ab9db365047fc1e8e8db1ced5f1
tigerfarm <tigerfarm@gmail.com>
2018-08-07 11:36:59 -0700

    Update

--- setvarsGroupSms.sh
+++
@@ -1,8 +0,0 @@
-echo "+++ Set variables."
-# ------------------------------------------------------------------------------
-export ACCOUNT_SID=your_account_SID
-export AUTH_TOKEN=your_account_auth_token
-export NOTIFY_SERVICE_SID=your_notify_service_SID
-export SYNC_SERVICE_SID=your_sync_service_SID
-php nEchoVarsSync.js
-# ------------------------------------------------------------------------------
