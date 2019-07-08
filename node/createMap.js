a6f0d3fb685e0bd03304a9c9cddb5f9c7922ec42
tigerfarm <tigerfarm@gmail.com>
2019-07-08 09:57:36 -0700

    updates

--- node/createMap.js
+++ node/createMap.js
@@ -1,19 +1,18 @@
 // https://www.twilio.com/docs/sync/api/maps#create-a-map
-console.log("+++ Create Map.");
 const accountSid = process.env.ACCOUNT_SID;
 const authToken = process.env.AUTH_TOKEN;
-const syncServiceSid = process.env.SYNC_SERVICE_SID;
-console.log("+ ACCOUNT_SID      :" + accountSid + ":");
-console.log("+ AUTH_TOKEN       :" + authToken + ":");
-console.log("+ SYNC_SERVICE_SID :" + syncServiceSid + ":");
 const client = require('twilio')(accountSid, authToken);
-client.sync.services(syncServiceSid)
-        .syncMaps
-        .create({ttl: 0, uniqueName: '+16508661233'})
-        .then((sync_map) => {
-            console.log("+ Created, Map SID: " + sync_map.sid);
-        })
-        .catch(function (error) {
-            console.log("- " + error);
-            // callback("- Error: " + error);
-        });
+const syncServiceSid = process.env.SYNC_SERVICE_SID;
+const syncMapName = process.env.SYNC_MAP_NAME;
+const syncMapItem = 'counterb';
+const syncMapItemCounterValue = 33;
+console.log("++ Create Sync Service:Map:Item: " + syncServiceSid + ":" + syncMapName + ":" + syncMapItem);
+let theData = {'counter': syncMapItemCounterValue};
+client.sync.services(syncServiceSid).syncMaps(syncMapName).syncMapItems
+    .create({key: syncMapItem, data: theData})
+    .then((sync_map_item) => {
+        console.log("+ Created.");
+    }).catch(function (error) {
+        console.log("- " + error);
+        // callback("- " + error);
+    });
